0e9da01a5de452eb4890a0388b5ae2e87d7e5890
Frederic Vogels <frederic.vogels@gmail.com>
2018-03-01 13:56:22 +0100

    Put Player into separate file

---
+++ Model/Reversi/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Reversi
+{
+    public abstract class Player
+    {
+        public static readonly Player ONE = new PlayerOne();
+
+        public static readonly Player TWO = new PlayerTwo();
+
+        public abstract Player OtherPlayer { get; }
+
+        private class PlayerOne : Player
+        {
+            public override Player OtherPlayer => TWO;
+        }
+
+        private class PlayerTwo : Player
+        {
+            public override Player OtherPlayer => ONE;
+        }
+    }
+}
--- Model/Reversi/ReversiBoard.cs
+++ Model/Reversi/ReversiBoard.cs
@@ -128,23 +128,4 @@ namespace Model.Reversi
             }
         }        
     }
-
-    public abstract class Player
-    {
-        public static readonly Player ONE = new PlayerOne();
-
-        public static readonly Player TWO = new PlayerTwo();
-
-        public abstract Player OtherPlayer { get; }
-
-        private class PlayerOne : Player
-        {
-            public override Player OtherPlayer => TWO;
-        }
-
-        private class PlayerTwo : Player
-        {
-            public override Player OtherPlayer => ONE;
-        }
-    }
 }
