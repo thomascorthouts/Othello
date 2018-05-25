69aa35dc2eeb9b4d8edf7ba2f4d675736aeafb8e
Thomas Corthouts <thomas.corthouts@icloud.com>
2018-05-25 23:05:06 +0200

    Added functionalities for showing score and current player.

---
+++ View/ActivePlayerToBackgroundBrush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace View
+{
+    class ActivePlayerToBackgroundBrush : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            bool active = (bool)value;
+
+            if (value != null && active)
+            {
+                return Brushes.Aqua;
+            }
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ViewModel/BoardViewModel.cs
+++ ViewModel/BoardViewModel.cs
@@ -24,10 +24,40 @@ namespace ViewModel
                 rows.Add(new BoardRowViewModel(gameCell, i));
             }
 
+            scorePlayer1 = gameCell.Derive(g => g.Board.CountStones(Player.BLACK));
+            scorePlayer2 = gameCell.Derive(g => g.Board.CountStones(Player.WHITE));
+            FirstPlayersTurn = gameCell.Derive(g => g.CurrentPlayer == Player.BLACK);
+            SecondPlayersTurn = gameCell.Derive(g => g.CurrentPlayer == Player.WHITE);
         }
 
-        public Cell<ReversiGame> gameCell { get; set; }
+        public Cell<bool> FirstPlayersTurn { get; set; }
 
+        public Cell<bool> SecondPlayersTurn { get; set; }
+        public Cell<ReversiGame> gameCell { get; set; }
+        
+        public Cell<int> scorePlayer1 { get; set; }
+
+        public Cell<int> scorePlayer2 { get; set; }
+
+        public string PlayerName1 {
+            get
+            {
+                return "Dirk";
+            }
+            set
+            {
+            }
+        }
+
+        public string PlayerName2 {
+            get
+            {
+                return "Jos";
+            }
+            set
+            {
+            }
+        }
 
         public List<BoardRowViewModel> Rows
         {
